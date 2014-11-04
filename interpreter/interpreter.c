fc6f45244536f98a6a85404790cb70eea4fa098d
zqiu <zqiu1994@gmail.com>
2014-11-04 00:22:28 -0500

    have a somewhat primitive version now. is able to interpret the sql file adequitely. Needs to still cover some corner cases and definitely needs a speedup

--- interpreter/interpreter.c
+++ interpreter/interpreter.c
@@ -5,7 +5,7 @@
 
 void writedata(FILE *readf, int numtoread);
 
-#define READSIZE 800
+#define READSIZE 1000
 #define	WRITESIZE 300
 #define PL_TITLE_MAX 255
 #define DEBUG
@@ -41,61 +41,94 @@ void writedata(FILE *readf, int numtoread){
 	while(readbuf[0] != 'I'){
 		fgets(readbuf,READSIZE,readf);
 	}
-	currentwritefile = fopen("0.txt","w+");
+	currentwritefile = fopen("0.txt","w");
+	/*while(1){
+		printf(readbuf);
+		fgets(readbuf,READSIZE,readf);
+		if(readbuf[0] == 'I'){
+			return;
+		}
+	}*/
 	for(i = 0; i < numtoread; ++i){
 		itr = NULL;
 		sentenial = 0;
 		while(!sentenial){
-			//start our our line
-			if(!itr){
+			if(!itr){//start of our line
 				itr = strtok(readbuf,"(");
 				printf("on line %d",i);
+#ifdef DEBUG
+			printf("%s\n",itr);
+#endif			
 			}else{//we have already advanced to the next element
 				itr = strtok(NULL,"(");
+				//means the previous operation itr = strtok(NULL,"\'");
+				//reached the eob. so pl_title was cut off
+				if(!itr){
+					strcpy(pl_title_partial,placeholder);
+					fgets(readbuf,READSIZE,readf);
+					if(readbuf[0] != '\''){
+						itr = strtok(readbuf,"\'");
+						placeholder = readbuf;
+						sprintf(writebuf,"%d,%d,%s%s\n",pl_from,pl_namespace,pl_title_partial,placeholder);
+						fwrite(writebuf, sizeof(char), strlen(writebuf), currentwritefile);
+						itr = strtok(NULL,"(");
+					}else{//inclase the buffer stopped right before the '
+						itr = strtok(readbuf+1,"\'");
+					}
+				}else{//write the data we have
+					sprintf(writebuf,"%d,%d,%s\n",pl_from,pl_namespace,placeholder);
+					fwrite(writebuf, sizeof(char), strlen(writebuf), currentwritefile);
+				}
 			}
-			//last char in our buffer is either )or, we don't care for it.
+			
+			itr = strtok(NULL,",");
+			//previous strtok(NULL,"(") reached eob
 			if(!itr){
 				fgets(readbuf,READSIZE,readf);
 				if(readbuf[0] == 'I'){
 					sentenial = 1;
-					break;
+					return;
+				}
+				if(readbuf[0] != '('){
+					itr = strtok(readbuf,"(");
+					itr = strtok(NULL,",");
+				}else{//incase the buffer stopped right before the (
+					itr = strtok(readbuf+1,",");
 				}
 			}
-#ifdef DEBUG
-			printf("%s\n",itr);
-#endif
 			pl_from = atoi(itr);
 			
 			itr = strtok(NULL,",");
-			//means our pl_from was cut
+			//previous strtok(NULL,",") reached eob. So pl_from was cutoff
 			if(!itr){
 				fgets(readbuf,READSIZE,readf);
-				itr = strtok(readbuf,"(");
-				scratch = atoi(readbuf);
-				pl_from = pl_from*pow(10.0,(float)strlen(readbuf)) + scratch;
+				//printf("%s\n",readbuf);
+				if(readbuf[0] != ','){
+					itr = strtok(readbuf,",");
+					scratch = atoi(readbuf);
+					pl_from = pl_from*pow(10.0,(float)strlen(readbuf)) + scratch;
+					itr = strtok(NULL,",");
+					//printf("got here %d\n",pl_from);
+				}else{//incase the buffer had stopped right before the ,
+					itr = strtok(readbuf+1,",");
+				}
 			}
 			pl_namespace = atoi(itr);
 			
 			itr = strtok(NULL,"\'");
-			//our pl_name_space was cut
+			//previous strtok(NULL,",") reached eob our pl_name_space was cutoff
 			if(!itr){
 				fgets(readbuf,READSIZE,readf);
-				itr = strtok(readbuf,"\'");
-				scratch = atoi(readbuf);
-				pl_namespace = pl_namespace*pow(10.0,(float)strlen(readbuf)) + scratch;
-			}			
+				if(readbuf[0] != ','){
+					itr = strtok(readbuf,",");
+					scratch = atoi(readbuf);
+					pl_namespace = pl_namespace*pow(10.0,(float)strlen(readbuf)) + scratch;
+					itr = strtok(NULL,"\'");
+				}else{//incase the buffer had stopped right before the '
+					itr = strtok(readbuf+1,"\'");
+				}
+			}		
 			placeholder = itr;
-			
-			itr = strtok(NULL,"\'");
-			//our pl_title was cut
-			if(!itr){
-				strcpy(pl_title_partial,placeholder);
-				fgets(readbuf,READSIZE,readf);
-				itr = strtok(readbuf,"\'");
-				placeholder = readbuf;
-			}
-			sprintf(writebuf,"%d,%d,%s\n",pl_from,pl_namespace,placeholder);
-			fwrite(writebuf, sizeof(char), strlen(writebuf), currentwritefile);
 		}
 	}
 }
